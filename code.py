c804f1590c651b96588b51aa5d1137bcf5fde216
niccit <ntynen+github@gmail.com>
2025-06-28 17:32:25 -1000

    Finalized layout and input for weather blocks, added time and date, ensured reasonable update publishes - only on change.

--- code.py
+++ code.py
@@ -42,9 +42,16 @@ remote_mqtt = adafruit_minimqtt.adafruit_minimqtt.MQTT(
     , ssl_context=ssl_context
 )
 
-remote_feed = mqtt_remote_username + "/feeds/" + os.getenv("garage_door_remote_feed")
+# MQTT Feeds
+garage_door_feed = mqtt_remote_username + "/feeds/" + os.getenv("garage_door_remote_feed")
+garage_door_icon_feed = mqtt_remote_username + "/feeds/" + os.getenv("garage_door_icon_remote_feed")
 weather_feed = mqtt_remote_username + "/integration/weather/2796/current"
-time_feed = mqtt_remote_username + "/feeds/" + os.getenv("date_time_remote_feed")
+weather_icon_feed = mqtt_remote_username + "/feeds/" + os.getenv("weather_icon_remote_feed")
+pub_weather_feed = mqtt_remote_username + "/feeds/" + os.getenv("weather_remote_feed")
+date_feed = mqtt_remote_username + "/feeds/" + os.getenv("date_remote_feed")
+time_feed = mqtt_remote_username + "/feeds/" + os.getenv("time_remote_feed")
+
+# Date Time IO integration
 tz = os.getenv("TIMEZONE")
 datetime_url = f"https://io.adafruit.com/api/v2/" + mqtt_remote_username + "/integrations/time/strftime?x-aio-key=" + mqtt_remote_key + "&tz=" + tz
 time_url = datetime_url + "&fmt=%25H%3A%25M"
@@ -95,40 +102,137 @@ garage_door_sensor.direction = digitalio.Direction.INPUT
 garage_door_sensor.pull = digitalio.Pull.UP
 garage_door_switch = Debouncer(garage_door_sensor)
 garage_door_state = False
-garage_door_check_wait = 10   # 3600 for normal operation, lower for testng
 
 # Get the weather for our dashboard
 requests = adafruit_requests.Session(pool, ssl_context)
 weather_integration = IO_HTTP(mqtt_remote_username, mqtt_remote_key, requests)
 weather_report = weather_integration.receive_weather(2796)
 
+# Wait times for different events
+door_check_wait = 5         # We only send an event when things change, and we want to know when things change quickly
+weather_report_wait = 3600  # Weather doesn't change that fast, update once an hour, default 3600
+
+
+stored_date = None
+def get_date():
+    global stored_date
+    current_date = requests.get(date_url)
+    # We only want to publish the date once per day
+    if stored_date is None or stored_date != current_date.text:
+        print("the date has changed or we just started, publishing date")
+        remote_mqtt.publish(date_feed, current_date.text)
+        stored_date = current_date.text
+        current_date.close()
+
+stored_time = None
+def get_time():
+    global stored_time
+    current_time = requests.get(time_url)
+    if stored_time is None or stored_time != current_time.text:
+        print("the time has changed or we just started, publishing time")
+        remote_mqtt.publish(time_feed, current_time.text)
+        stored_time = current_time.text
+        current_time.close()
+
+
+
+last_report = None
+def get_weather():
+    global last_report
+    current_conditions = weather_report["current"]
+    condition = current_conditions["conditionCode"]
+    pub_condition = get_formatted_condition(condition)
+    temperature = current_conditions["temperature"]
+    wind_speed = current_conditions["windSpeed"]            # km/hr
+    wind_direction = current_conditions["windDirection"]
+    print("wind direction is", wind_direction)
+    direction = get_wind_direction(wind_direction)
+    humidity = (current_conditions["humidity"] * 100)
+    daylight = current_conditions["daylight"]
+    degree_symbol = '\u00b0'
+    if last_report is None or time.monotonic() > last_report + weather_report_wait:
+        print("Publishing weather report")
+        weather_for_dash = f"""{pub_condition}
+           {str(int(temperature))}{degree_symbol}C
+           {str(wind_speed)} km/hr {direction}
+           {str(int(humidity))}%
+        """
+        icon = get_weather_icon(condition,daylight)
+        remote_mqtt.publish(weather_icon_feed, icon)
+        remote_mqtt.publish(pub_weather_feed, weather_for_dash)
+        last_report = time.monotonic()
+
+def get_weather_icon(condition, daylight):
+    icon = None
+    if daylight:
+        if "cloud" in condition.lower():
+            icon = "w:day-cloudy"
+        elif "rain" in condition.lower():
+            icon = "w:day-rain"
+        elif "sun" in condition.lower():
+            icon = "w:day-sunny"
+    else:
+        if "cloud" in condition.lower():
+            icon = "w:night-cloudy"
+        elif "rain" in condition.lower():
+            icon = "w:night-rain"
+
+    return icon
+
+def get_formatted_condition(condition):
+    pub_condition = None
+    if "mostly" in condition.lower():
+        if "cloud" in condition.lower():
+            pub_condition = "Mostly Cloudy"
+        elif "sun" in condition.lower():
+            pub_condition = "Mostly Sunny"
+    else:
+        pub_condition = condition
+
+    return pub_condition
+
+def get_wind_direction(wind_direction):
+    if 0 <= wind_direction <= 44:
+        direction = '\u2191'
+    elif 45 <= wind_direction <= 89:
+        direction = '\u2197'
+    elif 90 <= wind_direction <= 134:
+        direction = '\u2192'
+    elif 135 <= wind_direction <= 179:
+        direction = '\u2198'
+    elif 180 <= wind_direction <= 224:
+        direction = '\u2193'
+    elif 225 <= wind_direction <= 269:
+        direction = '\u2199'
+    elif 270 <= wind_direction <= 314:
+        direction = '\u2190'
+    else:
+        direction = '\u2196'
+
+    return direction
+
+
+
+
 while True:
 
     # Publish message when state changes from open to closed and vice versa
     if garage_door_sensor.value:
         if garage_door_state is not garage_door_sensor.value:
-            print("garage door open", garage_door_sensor.value)
-            remote_mqtt.publish(remote_feed, 0)
+            print("Publishing garage door state change, now", garage_door_state)
+            remote_mqtt.publish(garage_door_feed, "Garage Door is Open")
+            remote_mqtt.publish(garage_door_icon_feed, "frown-o")
             garage_door_state = garage_door_sensor.value
     elif not garage_door_sensor.value:
         if garage_door_state is not garage_door_sensor.value:
-            print("garage door closed", str(garage_door_sensor.value))
-            remote_mqtt.publish(remote_feed, 1)
+            print("Publishing garage door state change, now", garage_door_state)
+            remote_mqtt.publish(garage_door_feed, "Garage Door is Closed")
+            remote_mqtt.publish(garage_door_icon_feed, "smile-o")
             garage_door_state = garage_door_sensor.value
 
-    current_conditions = weather_report["current"]
-    today_forecast=weather_report["forecast_minutes_5"]
-    current_date = requests.get(date_url)
-    print(current_date.text)
-    current_date.close()
-    current_time = requests.get(time_url)
-    print(current_time.text)
-    current_time.close()
-    print("It is {1} and the current temperature is {2} C.".format(current_conditions["asOf"], current_conditions["conditionCode"], current_conditions["temperature"]))
-    print("Humidity is {0}%".format(current_conditions["humidity"] * 100))
-    precipitationChance=today_forecast["precipitationChance"]
-    precipitationIntensity=today_forecast["precipitationIntensity"]
-    print("Currently, the chance of rain is {0}%".format(precipitationChance))
+    get_date()
+    get_time()
+    get_weather()
 
-    time.sleep(garage_door_check_wait)
+    time.sleep(door_check_wait)
 
