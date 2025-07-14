7287ff1344f8383c232757107e6afaa4b4abcc2b
niccit <ntynen+github@gmail.com>
2025-07-13 18:48:46 -1000

    garage_sensor code in its own file. pi_code is Python on Linux. Added pressure reading to weather report. Working on integrating calendar events.

---
+++ garage_sensor.py
@@ -0,0 +1,97 @@
+# SPDX-License-Identifier: MIT
+
+import os
+import time
+import board
+import digitalio
+import wifi
+from adafruit_debouncer import Debouncer
+import adafruit_minimqtt.adafruit_minimqtt
+from adafruit_minimqtt.adafruit_minimqtt import MMQTTException
+import adafruit_connection_manager
+
+# WiFi
+connected = False
+while not connected:
+    try:
+        wifi.radio.connect(os.getenv("CIRCUITPYTHON_WIFI_SSID"), os.getenv("CIRCUITPYTHON_WIFI_PASSWORD"))
+        print("Connected to WiFi", str(wifi.radio.ap_info.ssid) + "!")
+        connected = True
+    except ConnectionError:
+        print("Failed to connect to WiFi")
+
+# MQTT
+radio = wifi.radio
+pool = adafruit_connection_manager.get_radio_socketpool(radio)
+ssl_context = adafruit_connection_manager.get_radio_ssl_context(radio)
+
+# Remote
+mqtt_remote_server = os.getenv("mqtt_remote_server")
+mqtt_remote_username = os.getenv("mqtt_remote_username")
+mqtt_remote_key = os.getenv("mqtt_remote_key")
+
+remote_mqtt = adafruit_minimqtt.adafruit_minimqtt.MQTT(
+    broker=mqtt_remote_server
+    , username=mqtt_remote_username
+    , password=mqtt_remote_key
+    , socket_pool=pool
+    , ssl_context=ssl_context
+)
+
+# Feed for garage door sensor
+garage_door_feed = mqtt_remote_username + "/feeds/" + os.getenv("garage_door_remote_feed")
+garage_door_icon_feed = mqtt_remote_username + "/feeds/" + os.getenv("garage_door_icon_remote_feed")
+
+try:
+    print("trying to connect to remote MQTT broker")
+    remote_mqtt.connect()
+    print("connected to remote MQTT broker")
+except MMQTTException:
+    print("unable to connect to remote MQTT broker")
+    raise
+
+# Garage Door Sensor
+garage_door_sensor=digitalio.DigitalInOut(board.A3)
+garage_door_sensor.direction = digitalio.Direction.INPUT
+garage_door_sensor.pull = digitalio.Pull.UP
+garage_door_switch = Debouncer(garage_door_sensor)
+door_check_wait = 20 # 1800
+
+
+def get_publish_message(garage_status):
+    if garage_status:
+        msg = "Garage door is open"
+        msg_icon = "frown-o"
+    else:
+        msg = "Garage door is closed"
+        msg_icon = "smile-o"
+
+    return msg, msg_icon
+
+last_known_state = False
+startup = True
+print("hello world, garage door sensor coming online")
+while True:
+    status = garage_door_sensor.value
+
+    if last_known_state is not status and startup is False:
+        print("Garage door state has changed, publishing to MQTT")
+        message, icon = get_publish_message(status)
+        print("message is", message, "icon is", icon)
+        remote_mqtt.publish(garage_door_icon_feed, icon)
+        remote_mqtt.publish(garage_door_feed, message)
+        last_known_state = status
+    elif startup:
+        print("System has started, publishing to MQTT")
+        message, icon = get_publish_message(status)
+        print("message is", message, "icon is", icon)
+        remote_mqtt.publish(garage_door_icon_feed, icon)
+        remote_mqtt.publish(garage_door_feed, message)
+        startup = False
+        last_known_state = status
+    else:
+        print("Nothing has changed, not publishing to MQTT")
+
+    time.sleep(door_check_wait)
+
+
---
+++ pi_code.py
@@ -0,0 +1,329 @@
+# SPDX-License-Identifier: MIT
+import os
+from os import path
+from dotenv import load_dotenv
+import time
+from datetime import datetime
+from datetime import timezone
+import paho.mqtt.client as mqtt
+import requests
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from pygments.lexers import q
+
+# Set testing to True to turn off publish to MQTT
+# Good for active development while code is running on a different client
+testing = True
+
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+# load environment variables
+load_dotenv()
+
+# Remote MQTT connection data
+mqtt_remote_server = os.getenv("mqtt_remote_server")
+mqtt_remote_username = os.getenv("mqtt_remote_username")
+mqtt_remote_key = os.getenv("mqtt_remote_key")
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqtt_client.on_connect = on_connect
+mqtt_client.tls_set()
+mqtt_client.username_pw_set(username=mqtt_remote_username, password=mqtt_remote_key)
+
+print("Connecting to MQTT broker...")
+mqtt_client.connect(mqtt_remote_server, 8883, 60)
+
+
+# --- Date & Time for the dashboard clock --- #
+
+# MQTT feeds for date and time
+date_feed = mqtt_remote_username + "/feeds/" + os.getenv("date_remote_feed")
+time_feed = mqtt_remote_username + "/feeds/" + os.getenv("time_remote_feed")
+
+def do_publish(feed, data):
+    if not testing:
+        mqtt_client.publish(feed, data)
+    else:
+        print("TESTING:", data)
+
+
+# Get date from system clock
+# Store the date (1-31) and if the new query is different update the date to MQTT
+stored_date = None
+def get_date():
+    global stored_date
+    now = datetime.now()
+    current_date = now.strftime("%d")
+    publish_date = now.strftime("%A %d %B %Y")
+    if stored_date is None or stored_date != current_date:
+        print("updating calendar date on dashboard")
+        do_publish(date_feed, publish_date)
+        stored_date = current_date
+
+# Get the time from system clock
+# Update time to MQTT every minute
+stored_time = None
+def get_time():
+    global stored_time
+    now = datetime.now()
+    current_min = now.minute
+    publish_time = now.strftime("%H:%M")
+    if stored_time is None or stored_time != current_min:
+        print("updating time on dashboard")
+        do_publish(time_feed, publish_time)
+        stored_time = current_min
+
+# --- Weather, Air Quality, so2 (vog indicator) --- #
+
+# Per Openweathermap API query every 10 minutes for most accurate information
+weather_report_wait = 600  # Weather doesn't change that fast, update once every 10 minutes (600)
+calendar_report_wait = 21600 # Calendar shouldn't change that much, so only check every 6 hours
+
+# URLs to Openweathermap API
+weather_feed = f"https://api.openweathermap.org/data/2.5/weather?lat=" + os.getenv("latitude") +  "&lon=" + os.getenv("longitude") + "&appid=" + os.getenv("openweather_api_key") + "&units=metric"
+air_quality_feed = f"http://api.openweathermap.org/data/2.5/air_pollution?lat=" + os.getenv("latitude") + "&lon=" + os.getenv("longitude") + "&appid=" + os.getenv("openweather_api_key")
+# MQTT feeds for publishing data to dashboard
+weather_icon_feed = mqtt_remote_username + "/feeds/" + os.getenv("weather_icon_remote_feed")
+pub_weather_feed = mqtt_remote_username + "/feeds/" + os.getenv("weather_remote_feed")
+
+# Gather all the weather and air quality data and format it into a report
+last_report = None
+def get_weather():
+    global last_report
+    degree_symbol = '\u00b0'
+    info_spacer = '\u25AA'
+    daylight = False
+#
+    if last_report is None or time.monotonic() > last_report + weather_report_wait:
+        weather_request = requests.get(weather_feed)
+        weather = weather_request.json()
+        condition = (weather["weather"][0]["description"])
+        temperature = (weather["main"]["temp"])
+        feels_like = (weather["main"]["feels_like"])
+        wind_speed = (weather["wind"]["speed"])
+        wind_direction = (weather["wind"]["deg"])
+        direction = get_wind_direction(wind_direction)
+        humidity = (weather["main"]["humidity"])
+        query_time = (weather["dt"])
+        sunrise_unix = (weather["sys"]["sunrise"])
+        sunset_unix = (weather["sys"]["sunset"])
+        sunrise = format_time(sunrise_unix)
+        sunset  = format_time(sunset_unix)
+        air_quality, so2, so2_quality = get_air_quality()
+        pressure = (weather["main"]["pressure"])
+        pressure_indicator, publish_pressure = get_pressure_info(pressure)
+        if query_time < sunset_unix:
+            daylight = True
+        w_icon = get_weather_icon(condition,daylight)
+        try:
+            wind_gust = (weather["wind"]["gust"])
+            weather_for_dash = f"""\
+                {condition}
+                {str(int(temperature))}{degree_symbol}C feels like {int(feels_like)}{degree_symbol}C
+                wind speed {str(wind_speed)} m/sec {direction}
+                wind gust {str(wind_gust)} m/sec
+                humidity {str(int(humidity))}%
+                sunrise {sunrise}
+                sunset { sunset}
+                air quality {air_quality}
+                vog (so2) {so2} {info_spacer} {so2_quality}
+                pressure {publish_pressure} mmHg {pressure_indicator} 
+                """
+        except KeyError:
+            weather_for_dash = f"""\
+                {condition}
+                {str(int(temperature))}{degree_symbol}C feels like {int(feels_like)}{degree_symbol}C
+                wind speed {str(wind_speed)} m/sec {direction}
+                humidity {str(int(humidity))}%
+                sunrise {sunrise}
+                sunset {sunset}
+                air quality {air_quality}
+                vog (so2) {so2} {info_spacer} {so2_quality}
+                pressure {publish_pressure} mmHg {pressure_indicator} 
+                """
+            pass
+
+        print("updating weather report on dashboard")
+        do_publish(weather_icon_feed, w_icon)
+        do_publish(pub_weather_feed, weather_for_dash)
+        last_report = time.monotonic()
+
+# API call returns the timestamp in Unix time format
+# This method will reformat it to local time
+# Using for: local times for sunrise/sunset
+def format_time(timestamp):
+    local_time = time.localtime(timestamp)
+    return "{:02d}:{:02d}:{:02d}".format(local_time.tm_hour, local_time.tm_min, local_time.tm_sec)
+
+# Get appropriate icon to display for weather. Using Adafruit IO icons from FontAwesome and Weather Icons Project
+def get_weather_icon(condition, daylight):
+    if daylight:
+        if "cloud" in condition.lower():
+            icon = "w:day-cloudy"
+        elif "rain" in condition.lower():
+            icon = "w:day-rain"
+        elif "sun" in condition.lower():
+            icon = "w:day-sunny"
+        else:
+            icon = "sun-o"
+    else:
+        if "cloud" in condition.lower():
+            icon = "w:night-cloudy"
+        elif "rain" in condition.lower():
+            icon = "w:night-rain"
+        else:
+            icon = "moon-o"
+
+    return icon
+
+# Get the air quality and so2 levels
+def get_air_quality():
+    response = requests.get(air_quality_feed)
+    air_quality =  response.json()
+    aq = (air_quality["list"][0]["main"]["aqi"])
+    so2 = (air_quality["list"][0]["components"]["so2"])
+
+    # Using the basic 1-5 scale documented on Openweathermap API
+    if aq == 1:
+        air_quality = "good"
+    elif aq == 2:
+        air_quality = "fair"
+    elif aq == 3:
+        air_quality = "moderate"
+    elif aq == 4:
+        air_quality = "poor"
+    elif aq == 5:
+        air_quality = "very poor"
+    else:
+        air_quality = "unable to retrieve air quality"
+
+    # Using table provided on Openweathermap API
+    if 0 <= so2 < 20:
+        so2_quality = "good"
+    elif 20 <= so2 < 80:
+        so2_quality = "fair"
+    elif 80 <= so2 < 250:
+        so2_quality = "moderate"
+    elif 250 <= so2 < 350:
+        so2_quality = "poor"
+    elif so2 >= 350:
+        so2_quality = "very poor"
+    else:
+        so2_quality = "unable to retrieve so2 quality"
+
+    return air_quality, so2, so2_quality
+
+# Return unicode character for direction based on degrees
+def get_wind_direction(wind_direction):
+    if 0 <= wind_direction <= 44:
+        direction = '\u2191'
+    elif 45 <= wind_direction <= 89:
+        direction = '\u2197'
+    elif 90 <= wind_direction <= 134:
+        direction = '\u2192'
+    elif 135 <= wind_direction <= 179:
+        direction = '\u2198'
+    elif 180 <= wind_direction <= 224:
+        direction = '\u2193'
+    elif 225 <= wind_direction <= 269:
+        direction = '\u2199'
+    elif 270 <= wind_direction <= 314:
+        direction = '\u2190'
+    else:
+        direction = '\u2196'
+
+    return direction
+
+stored_pressure= None
+def get_pressure_info(pressure):
+    global stored_pressure
+    indicator = None
+    publish_pressure = round((pressure * 0.750061683), 2)
+
+    if stored_pressure is not None:
+        if pressure > stored_pressure:
+            indicator = '\u2BAC'
+        elif pressure < stored_pressure:
+            indicator = '\u2BAD'
+        else:
+            indicator = '\u00A0'
+        stored_pressure = pressure
+    else:
+        indicator = '\u00A0'
+        stored_pressure = pressure
+
+    return indicator, publish_pressure
+
+# MQTT feed for calendar
+calendar_feed = mqtt_remote_username + "/feeds/" + os.getenv("calendar_remote_feed")
+
+last_calendar_check = None
+def get_shared_calendar_events():
+    global last_calendar_check
+    pub_events = None
+
+    if last_calendar_check is None or time.monotonic() > last_calendar_check + calendar_report_wait:
+        creds = None
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+                creds = flow.run_local_server(port=0)
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+
+        try:
+            service = build("calendar", "v3", credentials=creds)
+
+            now = datetime.now(tz=timezone.utc).isoformat()
+            events_result = (
+                service.events().list(
+                    calendarId="snipcrthka3m1mbm501fa486l4@group.calendar.google.com",
+                    timeMin=now,
+                    maxResults=2,
+                    singleEvents=True,
+                    orderBy="startTime",
+                )
+                .execute()
+            )
+            events = events_result.get("items", [])
+
+            if not events:
+                pub_events = "No upcoming events found."
+                do_publish(calendar_feed, pub_events)
+            else:
+                pub_array = []
+                for event in events:
+                    start = event["start"].get("dateTime", event["start"].get("date"))
+                    event_to_add = start, event["summary"]
+                    pub_array.append(event_to_add)
+
+                publish_events = f"""\
+                    {pub_array[0]}
+                    {pub_array[1]}
+                """
+                do_publish(calendar_feed, publish_events)
+
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            pass
+
+        last_calendar_check = time.monotonic()
+
+print("hello world, home hub is starting up!")
+while True:
+    get_date()
+    get_time()
+    get_weather()
+    get_shared_calendar_events()
+
+    time.sleep(0.5)
