2cc2fa8919934231a2c6cd713ef7d4df0f5d888a
niccit <ntynen+github@gmail.com>
2025-09-10 16:53:04 -1000

    motion_detect - removed timestamps, they were for testing only. Added robustness around mqtt loop, implemented method to not send motion events when we're recording. driveway_camera - renamed to support adding a front door camera, added a separate encoder for capturing 30 second clip so we can use Ffmpeg encoder. Added support for determining if images and video capture will be sent to local file server, remote or both. Added comments to enhance code organization. changed some info logging to debug. added new method for naming files, what is returned is based on the caller. Added proper implementation of connecting to Dropbox with refresh_token. Since we can now copy to multiple storage locations added separate method to publish to MQTT when all capture tasks are complete. Fixed funky looking arrangement of the main loop.

--- driveway_camera.py
+++ driveway_camera.py
@@ -3,6 +3,7 @@ import base64
 import time
 import os
 import paramiko
+from paramiko.ssh_exception import AuthenticationException
 from scp import SCPClient
 import uuid
 from datetime import datetime
@@ -10,9 +11,9 @@ from dotenv import load_dotenv
 import paho.mqtt.client as mqtt
 import logging
 from libcamera import Transform
-from picamera2.outputs import PyavOutput, FileOutput
+from picamera2.outputs import PyavOutput, FfmpegOutput
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
+from picamera2.encoders import H264Encoder, MJPEGEncoder
 import dropbox
 from dropbox.exceptions import AuthError
 
@@ -21,13 +22,21 @@ from dropbox.exceptions import AuthError
 # Will provide live stream and publish still images to MQTT
 # Will upload image and video capture files to local and remote storage
 
+# --- General variables --- #
 # load .env
 load_dotenv()
 
-# Pi Camera Setup
+# Set testing to True to turn off publish to MQTT
+# Good for active development while code is running
+testing = False
+
+# Where to publish stills and video clips - local, remote, both
+# By default local
+storage = "local"
+
+# --- Pi Camera Setup --- #
 tuning = Picamera2.load_tuning_file("imx219_noir.json")
 picam = Picamera2(tuning=tuning)
-
 # Video file name - use the same one and overwrite
 tmp_video = "temp_video"
 video_encoding = "mp4"
@@ -37,38 +46,48 @@ main = {'size': (1920, 1080), 'format': 'YUV420'}
 lores = {'size': (1920, 1080), 'format': 'YUV420'}
 capture = {'size': (1280, 720), 'format': 'YUV420'}
 controls = ({'FrameRate': 30})
+# Live camera feed
 config = picam.create_video_configuration(main, controls=controls, lores=lores, display="lores", transform=Transform(hflip=False, vflip=True))
-picam.configure(config)
-# Video set up - stream and capture
-encoder = H264Encoder()
-output_live = PyavOutput("rtsp:192.168.68.70:8554/cam", format="rtsp")
-output_capture = FileOutput()
-encoder.output = [output_live, output_capture]
 # Snap a picture on motion detect
 still_config = picam.create_still_configuration(main)
 
-# Set testing to True to turn off publish to MQTT
-# Good for active development while code is running
-testing = False
+picam.configure(config)
 
+# Encoder and output configuration
+encoder = H264Encoder()
+encoder_capture = MJPEGEncoder()
+output_live = PyavOutput("rtsp:192.168.68.70:8554/cam", format="rtsp")
+output_capture = FfmpegOutput(filename)
+encoder.output = [output_live]
+encoder_capture.output = [output_capture]
+
+# --- Logging set up --- #
 # Set the log level based on if we're testing or not
-if testing:
-    LOG_LEVEL = logging.DEBUG
-else:
+if not testing:
     LOG_LEVEL = logging.INFO
+else:
+    LOG_LEVEL = logging.DEBUG
 
 # Set up the logger
 log_file = "/home/picam/sec_cam/picam.log"
 logging.basicConfig(filename=log_file, level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
-# SSH Connection
+# ---  SSH Connection --- #
 ssh_username = os.getenv("FILE_SERVER_USERNAME")
 ssh_password = os.getenv("FILE_SERVER_PASSWORD")
 file_server_ip = os.getenv("FILE_SERVER_IP")
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+logger.info("configuring SSH client")
+try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_connected = True
+except AuthenticationException as auth_err:
+    logger.error("Authentication failed {}".format(auth_err))
+    ssh_connected = False
+    pass
 
+# --- MQTT --- #
 # MQTT data for connecting
 mqtt_server = os.getenv('MQTT_LOCAL_SERVER')
 ca_cert_file = os.getenv('MQTT_CA_CERT')
@@ -84,17 +103,10 @@ recording_feed = os.getenv('LOCAL_RECORDING_ON_FEED')
 feeds_list=[]
 motion_feed = os.getenv('LOCAL_MOTION_FEED')
 feeds_list.append(motion_feed)
-
-# Where to publish stills and video clips - local, remote, both
-# By default local
-storage = "local"
-logger.info(f"storage options is {storage}")
-
-# Date and time, formatted. Used for upload filenaming
-current_date, current_time = None, None
+storage_feed = os.getenv('LOCAL_STORAGE_FEED')
+feeds_list.append(storage_feed)
 
 # --- MQTT methods for handling traffic --- #
-
 # Set up MQTT client and connect to broker
 def connect_mqtt(cname):
     client_id = f'{cname}-mqtt-client-{uuid.getnode()}'
@@ -113,7 +125,6 @@ def connect_mqtt(cname):
 
     return client
 
-# Connect method ro MQTT
 # What to do when the client connects
 def on_connect(client, userdata, flags, reason_code, properties):
     global is_recording
@@ -127,8 +138,6 @@ def on_connect(client, userdata, flags, reason_code, properties):
         else:
             logger.info("there are no feeds in the list")
 
-
-# Disconnect method for MQTT
 # Auto reconnect logic
 FIRST_RECONNECT_DELAY = 1
 RECONNECT_RATE = 2
@@ -154,20 +163,21 @@ def on_disconnect(client, userdata, rc):
 # Subscribe method for MQTT
 # What to do when the subscriber gets a message
 def on_message(client, userdata, msg):
-    global storage, current_date, current_time
+    global storage
     received_msg = msg.payload.decode("utf-8")
-    logger.info("message payload is %s for topic %s", received_msg, msg.topic)
+    logger.debug("message payload is %s for topic %s", received_msg, msg.topic)
     # Handle motion detection
     if "motion" in msg.topic:
         if "1" in received_msg:
-            logger.info(f"{client.client_id} received message {received_msg} for topic {msg.topic}")
-            current_date, current_time = get_date_time()
+            logger.debug(f"{client.client_id} received message {received_msg} for topic {msg.topic}")
+            time.sleep(1)
             start_recording()
         else:
-            logger.info(f"{client.client_id} received message {received_msg} for topic {msg.topic}")
+            logger.debug(f"{client.client_id} received message {received_msg} for topic {msg.topic}")
     # Handle storage assignment
-    if "file" in msg.topic:
+    if "storage" in msg.topic:
         storage = received_msg
+        logger.debug(f"storage is now {received_msg}")
 
 # Publish to MQTT
 # Since subsequent publishes are done we need to ensure the publishes happen at the time of call
@@ -175,7 +185,7 @@ def on_message(client, userdata, msg):
 # to do this we use the wait_for_publish method
 # If the testing flag is enabled, print what would normally be published to MQTT
 def do_publish(feed, data):
-    if testing:
+    if not testing:
         logger.info("Publishing to %s", feed)
         result = pub_mqtt.publish(feed, data)
         result.wait_for_publish()
@@ -185,8 +195,7 @@ def do_publish(feed, data):
         logger.debug("Would publish: Topic: %s. Payload: %s", str(feed), str(data))
         return "TESTING"
 
-# --- Non MQTT specific helpers --- #
-
+# --- General helpers --- #
 # Get the time from system clock and format it in human-readable formate
 def get_date_time():
     now = datetime.now()
@@ -205,7 +214,7 @@ def start_recording():
         logger.info("start recording finished, calling next task")
         motion_detected()
     else:
-        logger.info("start recording failed, not proceeding")
+        logger.error("start recording failed, not proceeding")
 
 # When motion is detected, capture and image, base64 encode it, and send to MQTT
 # Upon success, call capture_clip to grab a portion of the live feed and save it to a file
@@ -224,24 +233,23 @@ def motion_detected():
         logger.info("motion detected finished, calling next task")
         capture_clip()
     else:
-        logger.info("motion detected failed, not proceeding")
+        logger.error("motion detected failed, not proceeding")
 
 # Capture a portion of the live feed to save to a file
 # By default only send to local fileserver
 # Use MQTT to publish whether that should be remote or both
 # MQTT feed: monitoring.storage
 capture_run_time = os.getenv("VIDEO_CAPTURE_TIME")
-ffmpeg_filename = f"{filename}_encoded.{video_encoding}"
 def capture_clip():
     global is_recording
     logger.info("capture clip has been called")
     record = True
     end_time = time.monotonic() + int(capture_run_time)
-    output_capture.fileoutput = filename
-    output_capture.start()
+    picam.start_encoder(encoder_capture)
     while record:
         if time.monotonic() > end_time:
             output_capture.stop()
+            picam.stop_encoder(encoder_capture)
             record = False
     logger.info("capture clip finished, calling next task")
     if "local" in storage:
@@ -250,78 +258,119 @@ def capture_clip():
         copy_to_remote_server()
     if "both" in storage:
         copy_to_local_server()
+        time.sleep(2)
         copy_to_remote_server()
 
-# Just in case we get here without the current date and time being set
-if current_date is None or current_time is None:
-    current_date, current_time = get_date_time()
+# --- Copy to local and or remote fileservers --- #
 
-# Copy to local fileserver
-video_storage_path = os.getenv("VIDEO_STORAGE_PATH")
-local_file_storage_path = os.getenv("LOCAL_FILE_STORAGE_PATH")
-local_video_file = local_file_storage_path + "/" + ffmpeg_filename
-remote_video_file = video_storage_path + "/" + "video_capture_" + current_date + "-" + current_time + "." + video_encoding
-local_image_file = local_file_storage_path + "/" + image_filename
-remote_image_file = video_storage_path + "/" + "image_" + current_date + "-" + current_time + image_type
+# Set the paths for the local and remote files and rename remote files for storage
+def name_files_to_copy(caller):
+    current_date, current_time = get_date_time()
+    local_file_location = os.getenv("LOCAL_FILE_LOCATION")
+    local_file_storage_path = os.getenv("LOCAL_STORAGE_PATH")
+    remote_file_storage_path = os.getenv("REMOTE_STORAGE_PATH")
+    local_video_file = local_file_location + "/" + filename
+    local_image_file = local_file_location + "/" + image_filename
+    remote_video_file = remote_file_storage_path + "/" + "video_capture_" + current_date + "-" + current_time + "." + video_encoding
+    remote_image_file = remote_file_storage_path + "/" + "image_" + current_date + "-" + current_time + image_type
+    local_storage_video = local_file_storage_path + "/" + "video_capture_" + current_date + "-" + current_time + "." + video_encoding
+    local_storage_image = local_file_storage_path + "/" + "image_" + current_date + "-" + current_time + image_type
+
+    if caller == "local":
+        return local_video_file, local_image_file, local_storage_video, local_storage_image
+    elif caller == "remote":
+        return local_video_file, local_image_file, remote_video_file, remote_image_file
+    else:
+        return None
+
+# Copy files to local file server
 def copy_to_local_server():
+    global ssh_connected
     logger.info("copy video to local server has been called")
-    try:
-        ssh.connect(file_server_ip, username=ssh_username, password=ssh_password)
-        scp = SCPClient(ssh.get_transport())
-        scp.put(local_video_file, remote_video_file)
-        scp.put(local_image_file, remote_image_file)
-        logger.info("successfully copied video and still shot to local server!")
-    except Exception as err:
-        logger.error("failed to copy video to local server! %s", err)
-    finally:
-        ssh.close()
+    files = name_files_to_copy("local")
+    local_video, local_image, remote_video, remote_image = files
+    if ssh_connected:
+        try:
+            ssh.connect(file_server_ip, username=ssh_username, password=ssh_password)
+            scp = SCPClient(ssh.get_transport())
+            scp.put(local_video, remote_video)
+            scp.put(local_image, remote_image)
+            logger.info("successfully copied video and still shot to local server!")
+        except Exception as err:
+            logger.error("failed to copy video to local server! %s", err)
+        finally:
+            ssh.close()
+    else:
+        logger.error("SSH connection not active, not attempting to copy to local server!")
+
+    end_recording("local")
 
 # Copy to Dropbox, the remote file service
 proceed = False
 access_token = os.getenv("DROPBOX_ACCESS_TOKEN")
 refresh_token = os.getenv("DROPBOX_REFRESH_TOKEN")
+app_key = os.getenv("DROPBOX_APP_KEY")
+app_secret = os.getenv("DROPBOX_APP_SECRET")
 def copy_to_remote_server():
     global proceed
     logger.info("copy to remote server has been called")
+    files = name_files_to_copy("remote")
+    local_video, local_image, remote_video, remote_image = files
     dbx = None
     # Try to connect with access and refresh tokens, report if failure but allow application to continue
     try:
-        dbx = dropbox.Dropbox(access_token)
-        dbx._oauth2_refresh_token = refresh_token
+        dbx = dropbox.Dropbox(
+            app_key=app_key,
+            app_secret=app_secret,
+            oauth2_refresh_token=refresh_token
+        )
+        logger.info("successfully authenticated with Dropbox")
         proceed = True
     except AuthError as err:
         print("Error connecting to Dropbox with access token: {}".format(err))
         pass
     # If successfully connect, publish still and video to Dropbox
     if proceed:
-        global remote_image_file, remote_video_file
         # Create valid image file for upload
-        with open(local_image_file, 'rb') as still:
+        with open(local_image, 'rb') as still:
             image_upload_file = still.read()
         still.close()
         # Create valid video file for upload
-        with open(local_video_file, 'rb') as video:
+        with open(local_video, 'rb') as video:
             video_upload_file = video.read()
         video.close()
         # Attempt to upload to Dropbox, report if failure but let application to continue
         try:
-            dbx.files_upload(image_upload_file, f"/Driveway/image_capture_{current_date}-{current_time}.{image_type}")
+            dbx.files_upload(image_upload_file, remote_image)
             time.sleep(2)
-            dbx.files_upload(video_upload_file, f"/Driveway/video_capture_{current_date}-{current_time}.{video_encoding}")
+            dbx.files_upload(video_upload_file, remote_video)
         except Exception as err:
-            print("Error uploading still and video capture files: {}".format(err))
+            logger.error("Error uploading still and video capture files: {}".format(err))
             pass
         finally:
             logger.info("uploaded still and video capture to remote server")
+
+    end_recording("remote")
+
+def end_recording(caller):
     # Let subscribers know recording is complete and any new motion can be detected
-    result = do_publish(recording_feed, 0)
-    if result:
-        logger.info("All motion detected tasks complete")
+    if storage == "both":
+        if caller == "remote":
+            logger.info(f"Publishing to {storage} per {caller}")
+            result = do_publish(recording_feed, 0)
+            if result:
+                logger.info("All motion detected tasks complete")
+            else:
+                logger.error("All motion detected tasks did not complete")
     else:
-        logger.info("All motion detected tasks did not complete")
+        logger.info(f"Publishing to {storage} per {caller}")
+        result = do_publish(recording_feed, 0)
+        if result:
+            logger.info("All motion detected tasks complete")
+        else:
+            logger.error("All motion detected tasks did not complete")
 
-# --- Prep for start of main --- #
-
+# --- Pre-launch steps --- #
 # Connect to MQTT for publish and subscribe
 logger.info("Connecting publish MQTT client")
 pub_mqtt = connect_mqtt("pub")
@@ -334,13 +383,14 @@ picam.start_encoder(encoder)
 picam.start()
 time.sleep(5)
 
-try:
-    while True:
+# --- Launch --- #
+while True:
+    try:
         # Ensure the clients are active
         pub_mqtt.loop_start()
         sub_mqtt.loop_start()
         time.sleep(0.25)
-except KeyboardInterrupt:
-    # If receive keyboard interrupt, stop camera
-    picam.stop()
-    logger.info("Camera stopped")
+    except KeyboardInterrupt:
+        # If receive keyboard interrupt, stop camera
+        picam.stop()
+        logger.info("Camera stopped")
