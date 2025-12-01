f1b840b2bf8200ef272b7d6b268750ac39e27c2f
niccit <ntynen+github@gmail.com>
2025-11-30 14:03:55 -1000

    Added check for established WAN connection. This prevents error if for some reason the internet is unreachable. When this is the case the code will not try any network dependent tasks. Refactored to use local MQTT server Refactored to use circuitpy_helpers for common workflows Added new publish of sunset to a new MQTT feed - used for LED lighting Refactored multi-line publishes to use JSON format for easier manipulation in Node-Red Dashboard TO-DO once garage door sensor is on-line refactor montior_garage_notification

--- pi_code.py
+++ pi_code.py
@@ -2,6 +2,8 @@
 import os
 import time
 import requests
+import uuid
+import json
 from dotenv import load_dotenv
 import paho.mqtt.client as mqtt
 from google.auth.transport.requests import Request
@@ -11,8 +13,9 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from pygments.lexers import q
 import logging
-from circuitpy_helpers.calendar_time_helpers import calendar_helper
-from circuitpy_helpers.calendar_time_helpers import time_helper
+from circuitpy_helpers.calendar_time_helpers import calendarHelper
+from circuitpy_helpers.calendar_time_helpers import timeHelper
+from circuitpy_helpers.network_helpers import wanChecker
 
 # --- Logging Configuration --- #
 
@@ -26,7 +29,7 @@ else:
     weather_wait = 600  # Weather doesn't change that fast, update once every 10 minutes (600)
 
 # Set up the logger
-logging.basicConfig(filename="hub.log", level=LOG_LEVEL)
+logging.basicConfig(filename="home-hub.log", level=LOG_LEVEL)
 logger = logging.getLogger(__name__)
 
 # --- Configuration --- #
@@ -36,11 +39,38 @@ load_dotenv()
 # list of feeds to subscribe to
 feeds_list = []
 
-# --- MQTT Methods --- #
+# --- MQTT Configuration --- #
+
+mqtt_server = os.getenv('MQTT_SERVER')
+mqtt_port = os.getenv('MQTT_PORT')
+ca_cert_file = os.getenv('MQTT_CA_CERT')
+client_pem = os.getenv('MQTT_CLIENT_PEM')
+client_username = os.getenv('MQTT_USERNAME')
+client_password = os.getenv('MQTT_PASSWORD')
+
+# --- MQTT methods for handling traffic --- #
+# Set up MQTT client and connect to broker
+def connect_mqtt(cname):
+    logger.info(f"Connecting to {cname} MQTT Broker!")
+    client_id = f'{cname}-mqtt-client-{uuid.getnode()}'
+    # Set Connecting Client ID
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.client_id = client_id
+    client.tls_set(ca_certs=ca_cert_file, certfile=client_pem, keyfile=None, keyfile_password=None)
+    client.username_pw_set(client_username, client_password)
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+    if "sub" in client.client_id:
+        client.connect_async(host=mqtt_server, port=int(mqtt_port), keepalive=60, clean_start=True)
+    else:
+        client.connect_async(mqtt_server, port=int(mqtt_port), keepalive=60)
+
+    return client
 
 # What to do when the client connects
 def on_connect(client, userdata, flags, reason_code, properties):
-    logger.info(f"Connected to {client.client_id} MQTT Broker!")
+    logger.debug(f"Connected to {client.client_id} MQTT Broker!")
     if "sub" in client.client_id:
         # Subscribe to any feeds
         if feeds_list:
@@ -50,85 +80,33 @@ def on_connect(client, userdata, flags, reason_code, properties):
         else:
             logger.info("there are no feeds in the list")
 
-# Disconnect method for MQTT
 # Auto reconnect logic
 FIRST_RECONNECT_DELAY = 1
 RECONNECT_RATE = 2
 MAX_RECONNECT_COUNT = 12
 MAX_RECONNECT_DELAY = 60
 def on_disconnect(client, userdata, rc):
-    logger.info("Disconnected with result code: %s", rc)
+    logger.error("Disconnected with result code: %s", rc)
     reconnect_count, reconnect_delay = 0, FIRST_RECONNECT_DELAY
     while reconnect_count < MAX_RECONNECT_COUNT:
         logger.info("Reconnecting in %d seconds...", reconnect_delay)
         time.sleep(reconnect_delay)
-
         try:
             client.reconnect()
             logger.info("Reconnected successfully!")
             return
         except Exception as err:
             logger.error("%s. Reconnect failed. Retrying...", err)
-
         reconnect_delay *= RECONNECT_RATE
         reconnect_delay = min(reconnect_delay, MAX_RECONNECT_DELAY)
         reconnect_count += 1
     logger.error("Reconnect failed after %s attempts. Exiting...", reconnect_count)
 
-# Subscribe logic when subscribing to an MQTT feed
-garage_door_state = 0
-def subscribe(client, topic):
-    def on_message(client, userdata, msg):
-        global garage_door_state
-        received_msg = msg.payload.decode("utf-8")
-        logger.debug("message payload is %s for topic %s", received_msg, msg.topic)
-        if "garage" in received_msg.lower():
-            garage_door_state = received_msg
-            logger.debug("garage door state is %s", garage_door_state)
-
-    client.subscribe(topic)
-    client.on_message = on_message
-
-# --- MQTT Configuration --- #
-
-mqtt_server = os.getenv("MQTT_SERVER")
-mqtt_port = os.getenv("MQTT_PORT")
-mqtt_username = os.getenv("MQTT_USERNAME")
-mqtt_key = os.getenv("MQTT_PASSWORD")
-mqtt_ca_cert = os.getenv("MQTT_CA_CERT")
-mqtt_client_cert = os.getenv("MQTT_CLIENT_PEM")
-
-# Connect to the specified MQTT server
-def connect_mqtt():
-    # Set Connecting Client ID
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
-    client.tls_set()
-    client.username_pw_set(username=mqtt_username, password=mqtt_key)
-
-    client.on_connect = on_connect
-    client.disconnect = on_disconnect
-    client.connect(mqtt_server, int(mqtt_port), 60)
-    return client
-
-# MQTT feeds - subs
-garage_notice = os.getenv("GARAGE_ALERT")
-garage_data_source = os.getenv("GARAGE_FEED")
-feeds_list.append(garage_data_source)
-
-# Connect to MQTT
-pub_mqtt_client = connect_mqtt()
-pub_mqtt_client.loop_start()
-sub_mqtt_client = connect_mqtt()
-
-# Subscribe to the feeds we need to take action
-def sub_feeds():
-    for feed in feeds_list:
-        subscribe(sub_mqtt_client, feed)
-        logger.info("subscribed to %s" % feed)
-
-    sub_mqtt_client.loop_start()
-
-# --- Date & Time for the dashboard clock --- #
+# Subscribe method for MQTT
+# What to do when the subscriber gets a message
+def on_message(client, userdata, msg):
+    received_msg = msg.payload.decode("utf-8")
+    logger.debug("message payload is %s for topic %s", received_msg, msg.topic)
 
 # MQTT feeds for date and time - pub
 date_feed = os.getenv("DATE_FEED")
@@ -139,10 +117,12 @@ time_feed = os.getenv("TIME_FEED")
 stored_date = None
 def get_date():
     global stored_date
-    current_date, publish_date = calendar_helper.get_date_from_system()
+    current_date, publish_date = calendarHelper.get_date_from_system()
     if stored_date is None or stored_date != current_date:
-        logger.debug("updating calendar date on dashboard")
-        do_publish(date_feed, publish_date)
+        logger.info(f"stored date is {stored_date}, current date is {current_date}")
+        logger.debug("updating date on dashboard")
+        logger.info(f"publishing to {date_feed} the date {publish_date}")
+        do_publish(date_feed, publish_date, True)
         stored_date = current_date
 
 # Get the time from system clock
@@ -150,10 +130,10 @@ def get_date():
 stored_time = None
 def get_time():
     global stored_time
-    now_min, publish_time = time_helper.get_current_time()
+    now_min, publish_time = timeHelper.get_current_time()
     if stored_time is None or stored_time != now_min:
         logger.debug("updating time on dashboard")
-        do_publish(time_feed, publish_time)
+        do_publish(time_feed, publish_time, True)
         stored_time = now_min
 
 # --- Weather, Air Quality, so2 (vog indicator) --- #
@@ -166,12 +146,14 @@ weather_feed = f"https://api.openweathermap.org/data/2.5/weather?lat=" + os.gete
 air_quality_feed = f"https://api.openweathermap.org/data/2.5/air_pollution?lat=" + os.getenv("LATITUDE") + "&lon=" + os.getenv("LONGITUDE") + "&appid=" + os.getenv("OPENWEATHER_API_KEY")
 # MQTT feeds for publishing data to dashboard - pub
 pub_weather_feed = os.getenv("WEATHER_FEED")
+sunset_feed = os.getenv("SUNSET_FEED")
 
 # Gather all the weather and air quality data and format it into a report
 last_report = None
 info_spacer = '\u25AA'
+sunset_sent = False
 def get_weather():
-    global last_report
+    global last_report, sunset_sent
     degree_symbol = '\u00b0'
     daylight = False
     if last_report is None or time.monotonic() > last_report + weather_report_wait:
@@ -186,41 +168,44 @@ def get_weather():
         humidity = (weather["main"]["humidity"])
         sunrise_unix = (weather["sys"]["sunrise"])
         sunset_unix = (weather["sys"]["sunset"])
-        sunrise = time_helper.format_time(sunrise_unix)
-        sunset  = time_helper.format_time(sunset_unix)
+        sunrise = timeHelper.format_time(sunrise_unix)
+        sunset  = timeHelper.format_time(sunset_unix)
         air_quality, so2, so2_quality = get_air_quality()
         pressure = (weather["main"]["pressure"])
         pressure_indicator, publish_pressure, rain_indicator = get_pressure_info(pressure)
         try:
             wind_gust = (weather["wind"]["gust"])
-            weather_for_dash = f"""\
-                {condition}
-                {str(int(temperature))}{degree_symbol}C feels like {int(feels_like)}{degree_symbol}C
-                wind speed {str(wind_speed)} m/sec {direction}
-                wind gust {str(wind_gust)} m/sec
-                humidity {str(int(humidity))}%
-                sunrise {sunrise}
-                sunset { sunset}
-                air quality {air_quality}
-                vog (so2) {so2} {info_spacer} {so2_quality}
-                pressure {publish_pressure} mmHg {pressure_indicator} {rain_indicator}
-                """
+            weather_for_dash = {"condition": f"{str(condition)}",
+                    "temperature": f"{str(int(temperature))}{degree_symbol}C feels like {int(feels_like)}{degree_symbol}",
+                    "wind": f"{str(wind_speed)} m/sec {direction}",
+                    "wind gust": f"{str(wind_gust)} m/sec",
+                    "humidity": f"{str(int(humidity))}%",
+                    "sunrise": f"{str(sunrise)}",
+                    "sunset": f"{str(sunset)}",
+                    "air quality": f"{str(air_quality)}",
+                    "vog": f"{float(so2)} {str(so2_quality)}",
+                    "pressure": f"{float(publish_pressure)} mmHg {pressure_indicator} {rain_indicator}",}
         except KeyError:
-            weather_for_dash = f"""\
-                {condition}
-                {str(int(temperature))}{degree_symbol}C feels like {int(feels_like)}{degree_symbol}C
-                wind speed {str(wind_speed)} m/sec {direction}
-                humidity {str(int(humidity))}%
-                sunrise {sunrise}
-                sunset {sunset}
-                air quality {air_quality}
-                vog (so2) {so2} {info_spacer} {so2_quality}
-                pressure {publish_pressure} mmHg {pressure_indicator} {rain_indicator}
-                """
+            weather_for_dash = {"condition": f"{str(condition)}",
+                    "temperature": f"{str(int(temperature))}{degree_symbol}C feels like {int(feels_like)}{degree_symbol}",
+                    "wind": f"{str(wind_speed)} m/sec {direction}",
+                    "humidity": f"{str(int(humidity))}%",
+                    "sunrise": f"{str(sunrise)}",
+                    "sunset": f"{str(sunset)}",
+                    "air quality": f"{str(air_quality)}",
+                    "vog": f"{float(so2)} {str(so2_quality)}",
+                    "pressure": f"{float(publish_pressure)} mmHg {pressure_indicator} {rain_indicator}",}
             pass
 
+
+        sunset_hr, sunset_minute, sunset_second = sunset.split(":")
+        sunset_info = f"{sunset_hr}:{sunset_minute}"
+
         logger.debug("updating weather report on dashboard")
-        do_publish(pub_weather_feed, weather_for_dash)
+        do_publish(pub_weather_feed, json.dumps(weather_for_dash), True)
+        if not sunset_sent:
+            do_publish(sunset_feed, sunset_info, True)
+            sunset_sent = True
         last_report = time.monotonic()
 
 
@@ -228,7 +213,6 @@ def get_weather():
 
 # MQTT feed for calendar - pub
 calendar_feed = os.getenv("CALENDAR_FEED")
-
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 calendar_report_wait = 7200 # Calendar shouldn't change that much, so only check every two hours
 
@@ -236,8 +220,9 @@ calendar_report_wait = 7200 # Calendar shouldn't change that much, so only check
 # Grab the next two events to publish
 # Note: if you change the number of events to grab, update the for loop that builds the publish string (until I figure out how to not need to do  this)
 last_calendar_check = None
+events_to_publish = None
 def get_shared_calendar_events():
-    global last_calendar_check
+    global last_calendar_check, events_to_publish
     events = []
     if last_calendar_check is None or time.monotonic() > last_calendar_check + calendar_report_wait:
         logger.debug("It's time to check the calendar")
@@ -257,7 +242,7 @@ def get_shared_calendar_events():
         for attempt in range(5):
             try:
                 service = build("calendar", "v3", credentials=creds)
-                now = time_helper.get_unix_time()
+                now = timeHelper.get_unix_time()
                 events_result = (
                     service.events().list(
                         calendarId="snipcrthka3m1mbm501fa486l4@group.calendar.google.com",
@@ -281,51 +266,54 @@ def get_shared_calendar_events():
                     logger.error("unable to retrieve events, abandoning")
             attempt += 1
 
+        pub_array = []
         if not events:
             pub_events = "No upcoming events found."
-            do_publish(calendar_feed, pub_events)
+            pub_array.append({"timestamp": "", "event": pub_events})
         else:
-            pub_array = []
             for event in events:
                 publish_event = event["summary"]
                 event_datetime = event["start"].get("dateTime")
                 if event_datetime is not None:
                     event_date, event_time = event_datetime.split("T")
                     event_year, event_month, event_day = event_date.split("-")
-                    full_month = calendar_helper.get_month_name(event_month)
+                    full_month = calendarHelper.get_month_name(event_month)
                     event_start_time, event_end_time = event_time.split("-")
                     event_time_hr, event_time_min, event_time_sec = event_start_time.split(":")
                     publish_datetime = f"{event_day} {full_month} {event_year} at {event_time_hr}:{event_time_min}"
-                    pub_array.append(publish_datetime + " " + info_spacer + " " + publish_event)
+                    pub_array.append({"timestamp": publish_datetime, "event": publish_event})
                 else:
                     event_date = event["start"].get("date")
                     event_year, event_month, event_day = event_date.split("-")
-                    full_month = calendar_helper.get_month_name(event_month)
+                    full_month = calendarHelper.get_month_name(event_month)
                     publish_date = f"{event_day} {full_month} {event_year}"
-                    pub_array.append(publish_date + " " + info_spacer + " " + publish_event)
+                    pub_array.append({"timestamp": publish_date, "event": + publish_event})
 
-            if len(pub_array) == 1:
-                message = f"{pub_array[0]}"
-            else:
-                message = f"""\
-                    {pub_array[0]}
-                    {pub_array[1]}"""
+        if len(pub_array) == 1:
+            pub_array.append({"timestamp": "", "event": ""})
 
-            logger.debug("Publishing calendar events")
-            do_publish(calendar_feed, message)
+        message = json.dumps(pub_array)
+
+        logger.debug("Publishing calendar events")
+        do_publish(calendar_feed, message, True)
 
         last_calendar_check = time.monotonic()
 
 # --- Supporting task handling methods --- #
 
 # Publish to MQTT
-def do_publish(feed, data):
+def do_publish(feed, data, retain=False):
     if not testing:
-        logger.debug("I am publishing %s to %s", data, feed)
-        pub_mqtt_client.publish(feed, data)
+        pub_mqtt_client.loop_start()
+        logger.info("I am publishing %s to %s", data, feed)
+        pub_mqtt_client.publish(feed, data, retain=retain)
+        pub_mqtt_client.loop_stop()
     else:
         logger.debug("TESTING:")
-        logger.debug("Would publish: Topic: %s. Payload: %s", str(feed), str(data))
+        if "sunset" in feed or "time" in feed:
+            logger.debug("Would publish: Topic: %s. Payload: %s. Retain: %s", str(feed), str(data), "true")
+        else:
+            logger.debug("Would publish: Topic: %s. Payload: %s. Retain: %s", str(feed), str(data), "false")
 
 # Get the air quality and so2 levels
 def get_air_quality():
@@ -418,9 +406,10 @@ def get_pressure_info(pressure):
 # Email me so that I know to close it, just in case I don't look at the home hub
 notified = False
 close_time = os.getenv("GARAGE_ALERT_TIME")
+garage_door_state = "closed"
 def monitor_garage_notification():
     global notified, close_time
-    cur_min, now = time_helper.get_current_time()
+    cur_min, now = timeHelper.get_current_time()
 
     # Publish that the garage door is open after 20:00
     # This will send an email alerting the need to close the door
@@ -439,21 +428,39 @@ def monitor_garage_notification():
 
 # Log a message indicating if we're in test mode
 if testing:
-    logger.debug("TESTING")
+    logger.debug("We are TESTING")
 else:
-    logger.info("LIVE")
+    logger.info("We are LIVE")
+
+# Connect to MQTT for publish and subscribe
+logger.info("Connecting publish MQTT client")
+pub_mqtt_client = connect_mqtt("pub")
+logger.info("Connecting subscribe MQTT client")
+sub_mqtt_client = connect_mqtt("sub")
 
 # Subscribe to any feeds
 if feeds_list:
-    logger.info("I have feeds to subscribe to")
-    sub_feeds()
+    logger.debug("I have feeds to subscribe to")
+
+# sub_mqtt_client.loop_start()
 
 logger.info("hello world, home hub is starting up!")
+wanCheck_counter = 0
+wan_state = True
 while True:
-    get_date()
-    get_time()
-    get_weather()
-    get_shared_calendar_events()
-    monitor_garage_notification()
 
+    if wanCheck_counter % 100 == 0:
+        wan_state = wanChecker.py_wan_active()
+        wanCheck_counter = 0
+        logger.info(f"wan state is {wan_state}")
+
+    if wan_state:
+        get_time()
+        get_date()
+        get_weather()
+        get_shared_calendar_events()
+        #    monitor_garage_notification()
+
+    wanCheck_counter += 1
     time.sleep(0.5)
+
